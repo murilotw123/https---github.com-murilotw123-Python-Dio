8e138812e95eaf034099d1b22139fd598d6803ac
Murilo Assis <Muassis38@gmail.com>
2024-10-10 13:15:15 -0300

    aula 04

---
+++ Python/Aula04-OperadoresLogicos.py
@@ -0,0 +1,17 @@
+print(True and True)
+print(True and False)
+print(True or False)
+print(False or False)
+print(True or True)
+
+saldo=1500
+saque=500
+limite=1000
+if(saldo>=saque and saldo>=limite):
+    print("Saque realizado com sucesso")
+else:
+    print("Saque não realizado")
+
+
+
+
